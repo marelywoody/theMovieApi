19915847a8e88efcad467c8c56fe32c654a42f0d
marelywoody <ros_ely_arc@hotmail.com>
2018-08-23 18:03:26 -0500

    pintar en pantalla la info

--- src/js/dom.js
+++ src/js/dom.js
@@ -1,19 +1,18 @@
-// let movies = document.getElementById('search-movie');
+let movies = document.getElementById('search-movie');
 let movie;
 const batman = document.getElementById('batman')
 const wonderWoman = document.getElementById('wonder-woman');
 const superMan = document.getElementById('super-man');
 const searchMovie = document.getElementById('buscar');
 const draw = document.getElementById('draw');
-// searchMovie.addEventListener('click', event => {
-//   movie = movies.value;
-//   getMovie(movie);
-// });
+searchMovie.addEventListener('click', event => {
+  movie = movies.value;
+  getMovie(movie);
+});
 
 batman.addEventListener('click', event => {
   movie = batman.value;
   getMovie(movie);
-  // draw = `<div>${drawMovie()}</div>`
 });
 
 wonderWoman.addEventListener('click', event => {
@@ -28,11 +27,9 @@ superMan.addEventListener('click', event => {
 
 const drawInfo = (data) => {
   let getInfo = drawMovie(data);
-  let info;
-  for (let i = 0; i < getInfo.length; i++) {
-    info = getInfo[i];
-    console.log(info);
-    
-      // draw.innerHTML = `<img src="${info.poster}">`;
-    }
+  draw.innerHTML = ' ';
+  getInfo.forEach((element) => {
+    console.log(element.poster);
+    draw.innerHTML += `<img src="${element.poster}"><p>${element.title}<br>${element.type}<br>${element.year}</p>`
+  });
 };
